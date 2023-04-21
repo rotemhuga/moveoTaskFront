cdd616f893243c703a44fc0b4498b67c1238d6c4
rotem huga <rotem.3007@gmail.com>
2023-04-21 10:00:11 +0300

    one code page completed influde live sharing

--- src/components/OneCodePage/OneCodePage.tsx
+++ src/components/OneCodePage/OneCodePage.tsx
@@ -22,6 +22,7 @@ const OneCodePage: React.FC = () => {
   return (
     <div className="APP">
       <h1 className="title-one-page">Share your Code Here</h1>
+      <div className="language-name">javascript</div>
       <div className="code-content">
         <Editor
           height="70vh"
@@ -43,45 +44,3 @@ const OneCodePage: React.FC = () => {
 };
 
 export default OneCodePage;
-
-
-
-// import "../OneCodePage/OneCodePage.css"
-// import {ICode } from "../../store/slices/codeSlice";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect, useState } from "react";
-// import { IRootState } from "../../store/store";
-// // import { ICodeState } from "../../store/store";
-// import { io } from 'socket.io-client';
-
-// const socket = io("http://localhost:8000")
-
-// const OneCodePage: React.FC = () => {
-//     const [message, setMessage] = useState("");
-//     const [messageReceived, setMessageReceived] = useState("");
-
-//     useEffect(() => {
-//         socket.on("recieve_message", (data:any) => {
-//         setMessageReceived(data.message)
-//         });
-//     }, [socket]);
-
-//         const handleInputChange = (event:any) => {
-//         setMessage(event.target.value);
-//         socket.emit("send_message", { message: event.target.value });
-//     };
-
-//     return (
-//         <div className="APP">
-//             <h1 className="title-one-page">Share your Code Here</h1>
-//             <input  
-//             placeholder='write your code here' 
-//             value={messageReceived}
-//             onChange={handleInputChange}
-//             className="input-content"
-//             type="text"
-//             />
-//         </div>
-//     )
-// }
-// export default OneCodePage
